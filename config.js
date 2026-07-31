833d4dbc7f6608402f9c54b9cf4d27f68f709588
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-07-31 17:22:36 -0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-window.APP_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxM2HzpzsA4hD6Jp3CEZ2i8HRkrqXAuvbk0VVGYx08qZ7Gx4NQX4JWXuJnyPdFKVbjOpw/exec",
-  DISTRICT: "Castelo de Sonhos",
-  FIELD: "Missão Oeste do Pará",
-  UNION: "União Norte Brasileira",
-  DIVISION: "Divisão Sul-Americana"
-};
