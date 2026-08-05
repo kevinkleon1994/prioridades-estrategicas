4391d5959af5cd49235531aaf1543fd73b135e41
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-08-05 15:12:17 -0300

    Update service worker cache version and assets

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,51 @@
-const CACHE="prioridades-v8-5-1";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./config.js","./manifest.webmanifest","./assets/logomarca_geometrica.png","./assets/icone_identidade.png","./assets/icone_lideranca.png","./assets/icone_novasgeracoes.png","./assets/icone_discipulado.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET") return;
-  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));
+const CACHE = "prioridades-v8-5-3";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./config.js",
+  "./manifest.webmanifest",
+  "./assets/icone_192.png",
+  "./assets/icone_512.png",
+  "./assets/logomarca_geometrica.png",
+  "./assets/icone_identidade.png",
+  "./assets/icone_lideranca.png",
+  "./assets/icone_novasgeracoes.png",
+  "./assets/icone_discipulado.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
